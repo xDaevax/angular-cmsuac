1634548950a79ee4bd0fb09c979ccc92315e7fca
xDaevax <xdaevax@gmail.com>
2021-11-23 16:17:17 -0500

    2 files added

---
+++ app/custom-timeline/custom-timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-timeline',
+  templateUrl: './custom-timeline.component.html',
+  styleUrls: ['./custom-timeline.component.css']
+})
+export class CustomTimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
