1364ae5293d50be593edbef91e79f0469ef8acac
xDaevax <xdaevax@gmail.com>
2021-11-23 16:32:50 -0500

    2 files modified, 1 added

--- app/custom-timeline/custom-timeline.component.ts
+++ app/custom-timeline/custom-timeline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+export type TimelineItem = {date: Date, label: string, type: string, subject: string};
 @Component({
   selector: 'app-custom-timeline',
   templateUrl: './custom-timeline.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomTimelineComponent implements OnInit {
 
+  public data: TimelineItem[];
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.data = [
+      {
+        date: new Date(),
+        label: 'Hello',
+        type: 'add',
+        subject: 'Test'
+      }
+    ];
   }
 
 }
